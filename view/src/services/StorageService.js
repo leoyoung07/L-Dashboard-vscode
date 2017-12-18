59ddf4a6b68223d84090f823f93900add9fe5533
Leo Young <leo.young@ssjovian.com>
2017-12-18 20:44:20 +0800

    todo list storage, keypress.enter

--- view/src/services/StorageService.js
+++ view/src/services/StorageService.js
@@ -1,34 +1,20 @@
 'use strict';
 export default class StorageService {
-  get(key) {
+  get(itemKey) {
     return new Promise(resolve => {
-      setTimeout(() => {
-        console.log(key);
-        resolve([{
-            id: 1,
-            text: "to do 1",
-            done: true
-          },
-          {
-            id: 2,
-            text: "to do 2",
-            done: false
-          },
-          {
-            id: 3,
-            text: "to do 3",
-            done: false
-          }
-        ]);
-      }, 1000);
+      let data = localStorage.getItem(itemKey);
+      console.log('get', data);
+      if (!data) {
+        data = '[]';
+      }
+      data = JSON.parse(data);
+      resolve(data);
     });
   }
-  update(key, data) {
+  update(itemKey, itemData) {
     return new Promise(resolve => {
-      setTimeout(() => {
-        console.log(key, data);
-        resolve();
-      }, 1000);
+      localStorage.setItem(itemKey, JSON.stringify(itemData));
+      resolve();
     });
   }
 
