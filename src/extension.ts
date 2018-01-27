c886f2089fef7da3500e1b94b0c7f891c94192dd
Leo Young <420760135@qq.com>
2018-01-27 19:54:01 +0800

    add explorer view l-toolset

--- src/extension.ts
+++ src/extension.ts
@@ -19,7 +19,7 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.showToolset', async () => {
+  const disposable = vscode.commands.registerCommand('extension.showToolset', async () => {
     // The code you place here will be executed every time your command is executed
 
     const indexHtml = 'file:///' + path.resolve(__dirname, '../view/index.html');
