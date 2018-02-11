f6b7c223de8d6c4f9c7426601cb3077cd82332e3
Leo Young <420760135@qq.com>
2018-02-11 18:10:29 +0800

    add network-tool view

---
+++ view/src/components/ViewNetworkTool.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="v-network-tool-wrapper">
+  </div>
+</template>
+<script>
+'use strict';
+export default {
+  components: {
+  },
+  computed: {
+  },
+  data () {
+    return {
+    };
+  },
+  methods: {
+  }
+};
+</script>
+<style>
+.v-network-tool-wrapper {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  font-family: "SFMono-Regular",Consolas,"Liberation Mono",Menlo,Courier,monospace;
+}
+
+</style>
---
+++ view/src/entries/network-tool/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="app app-black-theme">
+    <view-reg-exp-tool></view-reg-exp-tool>
+  </div>
+</template>
+
+<script>
+import ViewNetworkTool from '../../components/ViewNetworkTool.vue';
+
+export default {
+  name: 'App',
+  data () {
+    return {
+    };
+  },
+  components: {
+    ViewNetworkTool
+  },
+  methods: {
+  }
+};
+</script>
+<style>
+.app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  position: relative;
+}
+.app-black-theme {
+  background: #1E1E1E;
+  color: white;
+}
+</style>
